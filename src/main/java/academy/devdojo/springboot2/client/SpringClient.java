a9c4e2fae9ac97d38ea712bb02f62b8080f40ffc
Francisco Vieira <vieirafrancisco122@gmail.com>
2022-05-17 23:17:19 -0300

    Aula 28

--- src/main/java/academy/devdojo/springboot2/client/SpringClient.java
+++ src/main/java/academy/devdojo/springboot2/client/SpringClient.java
@@ -3,8 +3,7 @@ package academy.devdojo.springboot2.client;
 import academy.devdojo.springboot2.domain.Anime;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
@@ -28,5 +27,23 @@ public class SpringClient {
                 .exchange("http://localhost:8080/animes/all", HttpMethod.GET,
                         null, new ParameterizedTypeReference<>() {});
         log.info(exchange.getBody());
+
+//        Anime kingdom = Anime.builder().name("Kingdom").build();
+//        Anime kingdomSaved = new RestTemplate().postForObject("http://localhost:8080/animes/", kingdom, Anime.class);
+//        log.info("Saved anime {}", kingdomSaved);
+
+
+        Anime samuraiChampoo = Anime.builder().name("Samurai Champoo").build();
+        ResponseEntity<Anime> samuraiChampooSaved = new RestTemplate().exchange("http://localhost:8080/animes/",
+                HttpMethod.POST,
+                new HttpEntity<>(samuraiChampoo, createJsonHeader()),
+                Anime.class);
+        log.info("Saved anime {}", samuraiChampooSaved);
+    }
+
+    private static HttpHeaders createJsonHeader(){
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+        return httpHeaders;
     }
 }
